7acbdbdfb0a2bb63b016078ab2c8b05ffad7e5f5
sarvesh <kashyapsarvesh092@gmail.com>
2025-12-12 19:24:25 +0530

    stopwatch

---
+++ stopwatch/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import StopWatch from "./Pages/Stopwatch";
+function App() {
+
+  return (
+    <div className="appContainer"><StopWatch /></div>
+  )
+}
+
+export default App
---
+++ stopwatch/src/Pages/Stopwatch.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Stopwatch.module.css'
+
+const Stopwatch = () => {
+  return (
+    <div className={style.container}>
+      Stopwatch Page
+    </div>
+  )
+}
+
+export default Stopwatch
