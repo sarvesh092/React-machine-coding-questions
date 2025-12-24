743298424a701af4f978ed7192311e07a253bed6
sarvesh <kashyapsarvesh092@gmail.com>
2025-12-24 22:36:15 +0530

    tabs

---
+++ Tabs/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Tabs from "./components/Tabs";
+
+function App() {
+  return (
+    <div className="appContainer">
+      <Tabs />
+    </div>
+  );
+}
+
+export default App;
