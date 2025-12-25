f42d78dc9c125ced3351fc1023ac256dc2abbe84
sarvesh <kashyapsarvesh092@gmail.com>
2025-12-25 21:08:17 +0530

    search with debounce

---
+++ search/src/useDebounce.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export const useDebounce = (value, delay = 500) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebounceValue(value);
+    }, [delay]);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debounceValue;
+};
