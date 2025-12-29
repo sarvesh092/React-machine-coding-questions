33faff5848513e344ac24f2f9c9f26e230c5e7df
sarvesh <kashyapsarvesh092@gmail.com>
2025-12-29 19:34:19 +0530

    watchlist component

---
+++ watchlist/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Watchlist from "./watchlist";
+
+function App() {
+  return (
+    <>
+      <Watchlist />
+    </>
+  );
+}
+
+export default App;
