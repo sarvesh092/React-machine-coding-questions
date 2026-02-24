a384295077143033e7e58db5a18f9d01fdfcf457
sarvesh <kashyapsarvesh092@gmail.com>
2026-02-24 20:24:04 +0530

    autocomplete with debounce

--- AutoComplete/autocomplete/src/App.jsx
+++ AutoComplete/autocomplete/src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import Autocomplete from "./component/Autocomplete/Autocomplete";
 
 function App() {
-
   return (
     <>
+      <Autocomplete />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- AutoComplete/autocomplete/src/component/Autocomplete/Autocomplete.jsx
+++ AutoComplete/autocomplete/src/component/Autocomplete/Autocomplete.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+const FruitsData = [
+  "Apple",
+  "Banana",
+  "Cherry",
+  "Dragon Fruit",
+  "Elderberry",
+  "Fig",
+  "Grape",
+  "Honeydew",
+  "Kiwi",
+  "Lemon",
+  "Mango",
+  "Nectarine",
+  "Orange",
+  "Papaya",
+  "Quince",
+  "Raspberry",
+  "Strawberry",
+  "Tangerine",
+  "Ugli Fruit",
+  "Vanilla",
+  "Watermelon",
+  "Xigua",
+  "Yellow Passion Fruit",
+  "Zucchini",
+];
 
 const Autocomplete = () => {
+  const [input, setInput] = useState("");
+  const [sugestions, setSugestions] = useState([]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (input.length > 0) {
+        const filterSugestion = FruitsData.filter((item) =>
+          item.toLowerCase().includes(input.toLowerCase()),
+        );
+        setSugestions(filterSugestion);
+      } else {
+        setSugestions([]);
+      }
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setInput(value);
+  };
+  const handleSelect = (selectedItem) => {
+    setInput(selectedItem);
+    setSugestions([]);
+  };
+  console.log("input", input);
+  console.log("ss", sugestions);
   return (
     <div>
-      
+      <input
+        value={input}
+        placeholder="Search..."
+        onChange={(e) => handleChange(e)}
+      />
+      <ul>
+        {sugestions.map((item, index) => (
+          <li key={index} onClick={() => handleSelect(item)}>
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Autocomplete
+export default Autocomplete;
